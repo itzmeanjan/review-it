e5d046a24f2a8ed262154a2949cd45e8eeefb3a5
Anjan Roy <anjanroy@yandex.com>
2020-06-21 12:41:09 +0530

    Data structure for holding useful pieces of information, related to a {thing} that can be reviewed

--- contracts/Review.sol
+++ contracts/Review.sol
@@ -8,4 +8,17 @@ contract Review {
     constructor() public {
         author = msg.sender;
     }
+
+    // holds some useful pieces of information related to an item which can be
+    // reviewed by registered reviewers present in dApp ecosystem
+    struct Thing {
+        bytes32 id;
+        string name;
+        string description;
+        uint8 rate;
+        uint256 rateCount;
+        mapping(address => uint8) rates;
+        uint256 reviewCount;
+        mapping(address => string) reviews;
+    }
 }
